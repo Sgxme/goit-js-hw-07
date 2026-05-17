6a3bd06461ff63fd0e1b5ee4e69a39c00b485c84
Hladysh <sasagladis29@gmail.com>
2026-05-17 15:13:26 +0300

    goit-hwjs-07

---
+++ js/task-5.js
@@ -0,0 +1,15 @@
+const changeColorBtn = document.querySelector(".change-color");
+const colorText = document.querySelector(".color");
+
+changeColorBtn.addEventListener("click", () => {
+  const randomColor = getRandomHexColor();
+
+  document.body.style.backgroundColor = randomColor;
+  colorText.textContent = randomColor;
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
